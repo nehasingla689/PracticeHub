c5fe0a6470ce30ec125067452168b626cced6bcd
Amit Goel <Amit_Goel01@ad.infosys.com>
2017-09-21 18:04:19 +0530

    adding test cases

--- src/test/java/definition/StepDefinition.java
+++ src/test/java/definition/StepDefinition.java
@@ -4,6 +4,7 @@ package definition;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 
 public class StepDefinition {
@@ -12,17 +13,20 @@ public class StepDefinition {
     public void a_user_is_on_facebook_login_page() throws Throwable {
 
        System.out.println("inside given method");
+        Assert.assertEquals(false,true);
 
     }
 
     @When("^i enter valid details$")
     public void i_enter_valid_details() throws Throwable {
         System.out.println("inside  when method");
+        Assert.assertEquals(false,true);
     }
 
     @Then("^i should see the home page$")
     public void i_should_see_the_home_page() throws Throwable {
         System.out.println("inside then method");
+        Assert.assertEquals(false,true);
     }
 
 
